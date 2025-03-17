d8f867d63f0cb33fa19a5009c90a17c63b394aa6
Jakub Malinkiewicz <me@jmalinkiewicz.pl>
2025-03-17 21:20:04 +0100

    Initial Commit

---
+++ CodeCircuitApp/CodeCircuitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeCircuitAppApp.swift
+//  CodeCircuitApp
+//
+//  Created by Jakub Malinkiewicz on 17/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct CodeCircuitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
