2aca368d53ef6c625b253c84352973f5a66c445d
Jakub Malinkiewicz <me@jmalinkiewicz.pl>
2025-03-18 12:58:39 +0100

    create openWebsite View extension

---
+++ CodeCircuitApp/Extensions/View+Ext.swift
@@ -0,0 +1,25 @@
+//
+//  View+Ext.swift
+//  CodeCircuitApp
+//
+//  Created by Jakub Malinkiewicz on 18/03/2025.
+//
+
+import SwiftUI
+import SafariServices
+
+extension View {
+    func openWebsite(url: URL) {
+        let safariVC = SFSafariViewController(url: url)
+
+        // Find the nearest UIViewController in the SwiftUI hierarchy
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+           let rootViewController = windowScene.windows.first?.rootViewController {
+            var currentVC = rootViewController
+            while let presentedVC = currentVC.presentedViewController {
+                currentVC = presentedVC
+            }
+            currentVC.present(safariVC, animated: true, completion: nil)
+        }
+    }
+}
