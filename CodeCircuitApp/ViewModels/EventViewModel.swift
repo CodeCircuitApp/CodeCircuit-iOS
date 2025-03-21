3e1867295e4d7f13d0b4d8917d1e010d4d007160
Jakub Malinkiewicz <me@jmalinkiewicz.pl>
2025-03-21 16:02:40 +0100

    in case of a network error, present cached errors

--- CodeCircuitApp/ViewModels/EventViewModel.swift
+++ CodeCircuitApp/ViewModels/EventViewModel.swift
@@ -16,10 +16,31 @@ import SwiftUI
                switch result {
                case .success(let events):
                    self.events = events
+                   self.saveEvents()
                case .failure(let error):
                    print(error)
+                   self.loadEvents()
                }
            }
+        }
+    }
+    
+    func saveEvents() {
+        do {
+            let data = try JSONEncoder().encode(events)
+            try data.write(to: URL(fileURLWithPath: "/tmp/events.json"))
+        } catch {
+            print("Failed to save: \(error)")
+        }
+    }
+    
+    func loadEvents() {
+        do {
+            let data = try Data(contentsOf: URL(fileURLWithPath: "/tmp/events.json"))
+            let events = try JSONDecoder().decode([Event].self, from: data)
+            self.events = events
+        } catch {
+            print("Failed to load: \(error)")
         }
     }
 }
