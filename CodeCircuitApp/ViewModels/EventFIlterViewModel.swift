798ab17afcbf68cf458e2e091b7b4d5a8762427d
Jakub Malinkiewicz <me@jmalinkiewicz.pl>
2025-03-30 19:15:36 +0200

    use a filters model in EventFilterViewModel and add an updateFilters func

--- CodeCircuitApp/ViewModels/EventFIlterViewModel.swift
+++ CodeCircuitApp/ViewModels/EventFIlterViewModel.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 @Observable class EventFilterViewModel {
+    private(set) var filters = Filters()
+    
     var types: Set<Event.EventType> = []
     var locationTypes: Set<Event.LocationType> = []
     var educationStatuses: Set<Event.EducationStatus> = []
@@ -17,6 +19,18 @@ import SwiftUI
     var maxAge: Int? = nil
     var minTeamSize: Int? = nil
     var maxTeamSize: Int? = nil
+    
+    func updateFilters(types: Set<Event.EventType>, locationTypes: Set<Event.LocationType>, educationStatuses: Set<Event.EducationStatus>, paidSelected: Bool, freeSelected: Bool, includedSelected: Bool, excludedSelected: Bool, minAge: String, maxAge: String, minTeamSize: String, maxTeamSize: String) {
+        filters.types = types
+        filters.locationTypes = locationTypes
+        filters.educationStatuses = educationStatuses
+        filters.entryFee = getBoolValueOrNil(condition1: paidSelected, condition2: freeSelected)
+        filters.prizePool = getBoolValueOrNil(condition1: includedSelected, condition2: excludedSelected)
+        filters.minAge = !minAge.isEmpty ? Int(minAge) : nil
+        filters.maxAge = !maxAge.isEmpty ? Int(maxAge) : nil
+        filters.minTeamSize = !minTeamSize.isEmpty ? Int(minTeamSize) : nil
+        filters.maxTeamSize = !maxTeamSize.isEmpty ? Int(maxTeamSize) : nil
+    }
 
     func resetFilters() {
         types.removeAll()
@@ -28,5 +42,17 @@ import SwiftUI
         maxAge = nil
         minTeamSize = nil
         maxTeamSize = nil
+    }
+    
+    private func getBoolValueOrNil(condition1: Bool, condition2: Bool) -> Bool? {
+        if condition1 && condition2 {
+            return nil
+        } else if condition1 {
+            return true
+        } else if condition2 {
+            return false
+        } else {
+            return nil
+        }
     }
 }
