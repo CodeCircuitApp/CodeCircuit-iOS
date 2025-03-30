6dd5efd32d741b4856ba58b8527e98089cea044f
Jakub Malinkiewicz <me@jmalinkiewicz.pl>
2025-03-30 22:24:31 +0200

    add asURLQueryItems func to Filters struct

--- CodeCircuitApp/Model/Filters.swift
+++ CodeCircuitApp/Model/Filters.swift
@@ -17,4 +17,52 @@ struct Filters {
     var maxAge: Int? = nil
     var minTeamSize: Int? = nil
     var maxTeamSize: Int? = nil
+    
+    func asURLQueryItems() -> [URLQueryItem] {
+        var queryItems = [URLQueryItem]()
+        
+        func addSetQueryItems<T: Hashable>(name: String, set: Set<T>) where T: RawRepresentable, T.RawValue == String {
+            let joinedValues = set.map { $0.rawValue }.joined(separator: ",")
+            
+            queryItems.append(URLQueryItem(name: name, value: joinedValues))
+        }
+        
+        if !types.isEmpty {
+            addSetQueryItems(name: "type", set: types)
+        }
+        
+        if !locationTypes.isEmpty {
+            addSetQueryItems(name: "locationType", set: locationTypes)
+        }
+        
+        if !educationStatuses.isEmpty {
+            addSetQueryItems(name: "educationStatus", set: educationStatuses)
+        }
+        
+        if let entryFee = entryFee {
+            queryItems.append(URLQueryItem(name: "entryFee", value: String(entryFee)))
+        }
+        
+        if let prizePool = prizePool {
+            queryItems.append(URLQueryItem(name: "prizePool", value: String(prizePool)))
+        }
+        
+        if let minAge = minAge {
+            queryItems.append(URLQueryItem(name: "minAge", value: String(minAge)))
+        }
+
+        if let maxAge = maxAge {
+            queryItems.append(URLQueryItem(name: "maxAge", value: String(maxAge)))
+        }
+
+        if let minTeamSize = minTeamSize {
+            queryItems.append(URLQueryItem(name: "minTeamSize", value: String(minTeamSize)))
+        }
+
+        if let maxTeamSize = maxTeamSize {
+            queryItems.append(URLQueryItem(name: "maxTeamSize", value: String(maxTeamSize)))
+        }
+        
+        return queryItems
+    }
 }
