2b317fa180e9f9f48ffd55e56dd349ab68428179
Jakub Malinkiewicz <me@jmalinkiewicz.pl>
2025-06-09 22:51:51 +0200

    set map initial position to outzoomed center of poland

--- CodeCircuitApp/Screens/MapView.swift
+++ CodeCircuitApp/Screens/MapView.swift
@@ -10,9 +10,10 @@ import MapKit
 
 struct MapView: View {
     let coordinate: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 52.240225, longitude: 21.018661)
+    let initialPosition: MapCameraPosition = MapCameraPosition.camera(MapCamera(centerCoordinate: CLLocationCoordinate2D(latitude: 51.919400, longitude: 19.145100), distance: 1500000))
     
     var body: some View {
-        Map {
+        Map(initialPosition: initialPosition) {
             Marker("Warsaw University Hackathon 2025", coordinate: coordinate)
         }
     }
